bb0eeeb0938793deb302fd3e754d1c5babc9db0d
mfikrin <muhammad.fikrin1729@gmail.com>
2021-02-28 16:06:18 +0700

    partisi per semester

--- src/main.py
+++ src/main.py
@@ -89,39 +89,104 @@ def delete_dict(dict,key):
 
 # delete_arr(arr,'C3')
 print("deleye arr")
-print(arr)
+# print(arr)
 # print("deleeteteeee")
 # delete_key(dict,'C3')
 # print(dict)
 if is_N_preq_ada_zero(dict):
     print("HHOOOOOYYY")
 
-# semester = [[]for i in range(40)]
-semester = []
+
+def delete_arr(arr,take):
+    for content in arr:
+        course = content[0]
+        if course == take:
+            arr.remove(content)
+    return arr
+print("**************")
+print(arr)
+print("booooiiii")
+# arrTakes = ["C1","C2","C3","C4","C5","C6"]
+
+# def delete_dict_oy(dict,arrTakes):
+#     for take in arrTakes:
+#         for course in dict.keys():
+#             if course == take:
+#                 dict.pop(take)
+#     return dict
+def delete_oy(arr,arrTakes):
+    while (len(arrTakes) > 0):
+        for content in arr:
+            course = content[0]
+            for take in arrTakes:
+                if course == take:
+                    arr.remove(content)
+                    arrTakes.remove(take)
+    return arr
+
+# delete_oy(arr,arrTakes)
+# print(arr)
+# delete_oy(arr,arrTakes)
+# print(arr)
+arrTakes = ["C1","C2","C3","C4","C5","C6"]
+def kurang_oy(dict,arrTakes):
+    for course, info_course in dict.items():
+        for take in arrTakes:
+            if take in info_course['Arr_preq']:
+                info_course["N_preq"] -= 1
+# delete_dict_oy(dict,arrTakes)
+# print("TTTTTTTTTTTTTTTTTTTTTT")
+# print(dict)
+# kurang_oy(dict,arrTakes)
+# print(dict)
+semester = [[]for i in range(40)]
+# semester = []
+temporary = []
 i = -1
 if is_N_preq_ada_zero(dict):
+
     while dict != {} and is_N_preq_ada_zero(dict):
-        # i += 1
+        i += 1
+        print("print i woiiii : " + str(i))
+
+
+
+
+        temporary = []
         for content in arr:
+
             if dict[content[0]]["N_preq"] == 0:
                 take = content[0]
-                semester.append(take)
+                semester[i].append(take)
                 print("hmmmmm")
                 print(semester)
+                temporary.append(take)
+
 
                 # Mengurangi N_preq yang berhubungan dengan course take
-                for course, info_course in dict.items():
-                    if take in info_course['Arr_preq']:
-                        info_course["N_preq"] -= 1
-                        print("before pop")
-                        print(dict)
+                # for course, info_course in dict.items():
+                #     if take in info_course['Arr_preq']:
+                #         info_course["N_preq"] -= 1
+                #         print("before pop")
+                        # print(dict)
 
                 # Menghapus course yang telah diambil
                 delete_dict(dict,take)
                 # Menghapus arr course yang telah diambil
-                delete_arr(arr,take)
-
+                # delete_arr(arr,take)
+                print("arrr")
+                # print(arr)
+                print("dictt")
                 print(dict)
+
+            # print(temporary)
+
+        print("TEMPPPPORARY")
+        print(temporary)
+        kurang_oy(dict, temporary)
+        delete_oy(arr, temporary)
+        print("hauyoooo")
+        print(dict)
 
 else:
     print("Tidak ada urutan")
