f1835fd1fa2dbd002648faa986ace4740312694e
mfikrin <muhammad.fikrin1729@gmail.com>
2021-03-01 06:30:31 +0700

    add menu, error file handling

--- src/main.py
+++ src/main.py
@@ -3,12 +3,48 @@ Author  : Muhammad Fikri. N
 NIM     : 13519069
 Topik   : Topological Sort
 '''
-
+import os
 import util as ut
 
-filename = input("Masukkan nama file (eg : tc) (Tanpa ekstensi file) : ")
-test = open("../test/" + filename + ".txt", 'r')
-temp = test.read().splitlines() # baca file teks (dengan readlines yang sekaligus hapus \n)
+print("              ________  .__                   .___             ")
+print("               \_____  \ |  |   ____  ____   __| _/____ ___  ___")
+print("                /   |   \|  | _/ ___\/  _ \ / __ |/ __ \\\  \/  /")
+print("               /    |    \  |_\  \__(  <_> ) /_/ \  ___/ >    < ")
+print("               \_______  /____/\___  >____/\____ |\___  /__/\_ \\")
+print("                       \/          \/           \/    \/      \/\n")
+
+
+print("-----------------------------")
+print("|  Silahkan input nama file |")
+print("|        (eg : tc)          |")
+print("|   (Tanpa ekstensi file)   |")
+print("-----------------------------")
+
+print()
+
+# try:
+#     filename = input("Input nama file : ")
+#     test = open("../test/" + filename + ".txt", 'r')
+# except FileNotFoundError:
+#     print("File does not exist, ulangi lagi")
+#     filename = input("Input lg nama file : ")
+#     test = open("../test/" + filename + ".txt", 'r')
+# finally:
+#     print("NEXT")
+
+filename = input("Input nama file : ")
+path = "../test/" + filename + ".txt"
+
+isfile = os.path.isfile(path)
+# print(isfile)
+while (not isfile):
+    filename = input("Ulangi input nama file : ")
+    path = "../test/" + filename + ".txt"
+    isfile = os.path.isfile(path)
+
+test = open(path, 'r')
+
+temp = test.read().splitlines()  # baca file teks (dengan readlines yang sekaligus hapus \n)
 
 # delete string kosong (ketika file .txt memiliki banyak enter tp tidak ada record)
 ut.delete_arr_empty(temp)
@@ -33,19 +69,26 @@ dict = {}
 for i in range(len(arr)):
     N_preq = (len(arr[i][1:len(arr[i])]))
     Arr_preq = (arr[i][1:len(arr[i])])
-    dict[arr[i][0]] = {
-        "N_preq": N_preq,
-        "Arr_preq": Arr_preq
+    dict[arr[i][0]] = {                 # Key = Matkul
+        "N_preq": N_preq,               # Value = dictionary (key1 = N_preq (banyak prerequisite yang blm diambil)
+        "Arr_preq": Arr_preq            # key2 = Arr_preq (array dari matkul prerequisite))
     }
 
 semester = [[]for i in range(40)] # Asumsi maksimal 40 semester yang dapat ditampung
 
 temporary = []
 i = -1
+
+print()
+print("-----------------------------")
+print("|      Hasilnya adalah      |")
+print("|        ↓ ↓ ↓ ↓ ↓ ↓        |")
+print("-----------------------------")
+print()
+
 if ut.is_Npreq_have_zero(dict):
     while (dict != {} and ut.is_Npreq_have_zero(dict)):
         i += 1
-        print("print i woiiii : " + str(i))
 
         # untuk mengulang array temporary kembali kosong (berisi matkul yang diambil)
         temporary = []
@@ -65,8 +108,9 @@ if ut.is_Npreq_have_zero(dict):
 else:
     print("Tidak ada urutan yang mungkin")
     print("Pastikan kuliah dan prerequisite nya berupa Directed Acyclic Graph (DAG)")
+    print("Silahkan atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
 
-semester = [x for x in semester if x] #
+semester = [x for x in semester if x] # Untuk menghapus empty array dari sebuah array
 
 if len(semester) <= 8:
     for i in range(len(semester)):
@@ -84,14 +128,14 @@ else:
     print("-----------------------------")
 
     pil = int(input("Input pilihan : "))
-    while (pil != 1 or pil != 2):
+    while (pil != 1 and pil != 2):
         pil = int(input("Ulangi input pilihan, pastikan pilihan 1 atau 2 : "))
     if pil == 1:
         for i in range(len(semester)):
             print("Semester " + str(i + 1) + ": ", end="")
             print(ut.arrayToString(semester[i]))
-        print("Jangan lupa atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
+        print("\nJangan lupa atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
     else:
-        print("Silahkan atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
+        print("\nSilahkan atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
 
 
