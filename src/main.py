3e45f0869840ae596e43478b59a32c22c55aecaf
mfikrin <muhammad.fikrin1729@gmail.com>
2021-02-28 23:49:04 +0700

    cleaning dan add comments

--- src/main.py
+++ src/main.py
@@ -1,58 +1,35 @@
+'''
+Author  : Muhammad Fikri. N
+NIM     : 13519069
+Topik   : Topological Sort
+'''
 
-def delete_arr(arr,take):
-    for content in arr:
-        course = content[0]
-        if course == take:
-            arr.remove(content)
-    return arr
+import util as ut
 
-def delete_arr_empty(arr):
-    while ("" in arr):
-        arr.remove("")
-    return arr
-filename = input("Masukkan nama file (eg : tc) (Tanpa ekstensi file) : " )
+filename = input("Masukkan nama file (eg : tc) (Tanpa ekstensi file) : ")
 test = open("../test/" + filename + ".txt", 'r')
 temp = test.read().splitlines() # baca file teks (dengan readlines yang sekaligus hapus \n)
-print(temp)
-print(temp[0])
-print(temp[0])
 
-delete_arr_empty(temp)
-print(temp)
+# delete string kosong (ketika file .txt memiliki banyak enter tp tidak ada record)
+ut.delete_arr_empty(temp)
+
 temps = []
+
 for i in range(len(temp)):
     elm = temp[i].replace('.',"")
     elm = elm.replace(" ","")
     temps.append(elm)
-print(temps[0])
-print(len(temps[0]))
-
-
-print(temps)
-rows = len(temps)
 
 arr = []
+
 for i in range(len(temps)):
+    # mengubah array of string menjadi array of array string dgn delimiter koma (,)
     content = temps[i].split(',')
-    print(content)
+
     arr.append(content)
-# print("--------")
-# print(arr)
-# print((arr[0]))
-# print(arr[0][1])
-# print(arr[0][0])
-#
-# print("oooooooooo")
-# print(arr[3][1:(len(arr[3]))])
+
 dict = {}
 
-#
-# hmm = list(dict.fromkeys(arr))
-# print(hmm)
-
-print("ARRR")
-print(arr)
-print("ARRR")
 for i in range(len(arr)):
     N_preq = (len(arr[i][1:len(arr[i])]))
     Arr_preq = (arr[i][1:len(arr[i])])
@@ -61,134 +38,60 @@ for i in range(len(arr)):
         "Arr_preq": Arr_preq
     }
 
-print("hmm")
-print(dict)
+semester = [[]for i in range(40)] # Asumsi maksimal 40 semester yang dapat ditampung
 
-semester = []
-
-# for i in range(len)
-#     if (dict[])
-# print(dict["C1"].get("N_preq"))
-# while (dict[]["N_preq"] != 0):
-
-def is_N_preq_ada_zero(dict):
-    bool = False
-    for course, info_course in dict.items():
-        if info_course["N_preq"] == 0:
-            return True
-
-    return bool
-
-def delete_dict(dict,key):
-    for course in dict.keys():
-        if course == key:
-            dict.pop(key)
-            return dict
-
-
-
-# delete_arr(arr,'C3')
-print("deleye arr")
-# print(arr)
-# print("deleeteteeee")
-# delete_key(dict,'C3')
-# print(dict)
-if is_N_preq_ada_zero(dict):
-    print("HHOOOOOYYY")
-
-
-def delete_arr(arr,take):
-    for content in arr:
-        course = content[0]
-        if course == take:
-            arr.remove(content)
-    return arr
-print("**************")
-print(arr)
-print("booooiiii")
-# arrTakes = ["C1","C2","C3","C4","C5","C6"]
-
-# def delete_dict_oy(dict,arrTakes):
-#     for take in arrTakes:
-#         for course in dict.keys():
-#             if course == take:
-#                 dict.pop(take)
-#     return dict
-def delete_oy(arr,arrTakes):
-    while (len(arrTakes) > 0):
-        for content in arr:
-            course = content[0]
-            for take in arrTakes:
-                if course == take:
-                    arr.remove(content)
-                    arrTakes.remove(take)
-    return arr
-
-# delete_oy(arr,arrTakes)
-# print(arr)
-# delete_oy(arr,arrTakes)
-# print(arr)
-arrTakes = ["C1","C2","C3","C4","C5","C6"]
-def kurang_oy(dict,arrTakes):
-    for course, info_course in dict.items():
-        for take in arrTakes:
-            if take in info_course['Arr_preq']:
-                info_course["N_preq"] -= 1
-# delete_dict_oy(dict,arrTakes)
-# print("TTTTTTTTTTTTTTTTTTTTTT")
-# print(dict)
-# kurang_oy(dict,arrTakes)
-# print(dict)
-semester = [[]for i in range(40)]
-# semester = []
 temporary = []
 i = -1
-if is_N_preq_ada_zero(dict):
-
-    while dict != {} and is_N_preq_ada_zero(dict):
+if ut.is_Npreq_have_zero(dict):
+    while (dict != {} and ut.is_Npreq_have_zero(dict)):
         i += 1
         print("print i woiiii : " + str(i))
 
-
-
-
+        # untuk mengulang array temporary kembali kosong (berisi matkul yang diambil)
         temporary = []
-        for content in arr:
 
+        for content in arr:
             if dict[content[0]]["N_preq"] == 0:
                 take = content[0]
                 semester[i].append(take)
-                print("hmmmmm")
-                print(semester)
                 temporary.append(take)
 
+                # Menghapus course yang telah diambil pada dict
+                ut.delete_dict(dict,take)
 
-                # Mengurangi N_preq yang berhubungan dengan course take
-                # for course, info_course in dict.items():
-                #     if take in info_course['Arr_preq']:
-                #         info_course["N_preq"] -= 1
-                #         print("before pop")
-                        # print(dict)
-
-                # Menghapus course yang telah diambil
-                delete_dict(dict,take)
-                # Menghapus arr course yang telah diambil
-                # delete_arr(arr,take)
-                print("arrr")
-                # print(arr)
-                print("dictt")
-                print(dict)
-
-            # print(temporary)
-
-        print("TEMPPPPORARY")
-        print(temporary)
-        kurang_oy(dict, temporary)
-        delete_oy(arr, temporary)
-        print("hauyoooo")
-        print(dict)
+        ut.minus_Npreq(dict,temporary)
+        ut.delete_arr(arr,temporary)
 
 else:
-    print("Tidak ada urutan")
+    print("Tidak ada urutan yang mungkin")
+    print("Pastikan kuliah dan prerequisite nya berupa Directed Acyclic Graph (DAG)")
 
-print(semester)
+semester = [x for x in semester if x] #
+
+if len(semester) <= 8:
+    for i in range(len(semester)):
+        print("Semester " + str(i+1) + ": ", end ="")
+        print(ut.arrayToString(semester[i]))
+
+else:
+    print("Hasil proses melebihi 8 semester !!!")
+    print("Tidak boleh melebihi 8 semester :(")
+    print("Apakah tetap akan ditampilkan atau tidak perlu ?")
+    print("-----------------------------")
+    print("|         Opsi input        |")
+    print("|       1. YA               |")
+    print("|       2. TIDAK            |")
+    print("-----------------------------")
+
+    pil = int(input("Input pilihan : "))
+    while (pil != 1 or pil != 2):
+        pil = int(input("Ulangi input pilihan, pastikan pilihan 1 atau 2 : "))
+    if pil == 1:
+        for i in range(len(semester)):
+            print("Semester " + str(i + 1) + ": ", end="")
+            print(ut.arrayToString(semester[i]))
+        print("Jangan lupa atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
+    else:
+        print("Silahkan atur pilihan matkul kamu lagi, FIGHTING (～￣▽￣)～")
+
+
---
+++ src/util.py
@@ -0,0 +1,57 @@
+'''
+Author  : Muhammad Fikri. N
+NIM     : 13519069
+Topik   : Topological Sort
+'''
+
+# Untuk menghapus string kosong
+def delete_arr_empty(arr):
+    while ("" in arr):
+        arr.remove("")
+    return arr
+
+# Untuk menghapus elemen arr / matkul (berisi matkul beserta prereq) yang telah diambil (arrTakes)
+def delete_arr(arr,arrTakes):
+    while (len(arrTakes) > 0):
+        for content in arr:
+            course = content[0]
+            for take in arrTakes:
+                if course == take:
+                    arr.remove(content)
+                    arrTakes.remove(take)
+    return arr
+
+# Untuk menghapus record dictionary yang memiliki kunci = key
+def delete_dict(dict,key):
+    for course in dict.keys():
+        if course == key:
+            dict.pop(key)
+            return dict
+
+# Untuk mengurangi banyaknya prerequisite yang blm diambil
+def minus_Npreq(dict,arrTakes):
+    for course, info_course in dict.items():
+        for take in arrTakes:
+            if take in info_course['Arr_preq']:
+                info_course["N_preq"] -= 1
+
+# Untuk mengetahui apakah ada matkul yang banyak prerequisite yang blm diambil = 0
+def is_Npreq_have_zero(dict):
+    bool = False
+    for course, info_course in dict.items():
+        if info_course["N_preq"] == 0:
+            return True
+
+    return bool
+
+# Untuk convert array to string
+def arrayToString(arr):
+    string = ""
+    for i in range(len(arr)):
+        if (i < (len(arr)-1)):
+            string += arr[i]
+            string += ", "
+        else:
+            string += arr[i]
+            string += "."
+    return string
